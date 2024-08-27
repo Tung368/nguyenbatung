a699488b7a2ad636650f67b7e758c2f470d5d19c
Tung368 <nguyenbatung368@gmail.com>
2024-08-27 10:36:29 +0700

    Add files via upload

--- Homework.java
+++ Homework.java
@@ -1,10 +1,48 @@
-run:
-18 + 4 + 1 + 10 + 6 = 39
-4 + 9 + 16 = 29
-9 + 4 + 16 = 29
-14 + 8 + 14 = 36
-8 + 9 + 5 + 3 + 4 = 12
-sai! dap an la:  29
-12 + 15 + 12 + 5 + 17 = 2
-sai! dap an la:  61
-3 + 10 + 2 + 12 + 1 = 
+package Homework;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Homework {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int points = 0;
+        int wrongAttempts = 0;
+        final int maxWrongAttempts = 3;
+
+        while (wrongAttempts < maxWrongAttempts) {
+            int numNumbers = random.nextInt(4) + 2;
+            int[] numbers = new int[numNumbers];
+            int correctAnswer = 0;
+
+            for (int i = 0; i < numNumbers; i++) {
+                numbers[i] = random.nextInt(20) + 1;
+                correctAnswer += numbers[i];
+            }
+
+            StringBuilder problem = new StringBuilder();
+            for (int i = 0; i < numNumbers; i++) {
+                problem.append(numbers[i]);
+                if (i < numNumbers - 1) {
+                    problem.append(" + ");
+                }
+            }
+
+            System.out.print(problem.toString() + " = ");
+            int userAnswer = scanner.nextInt();
+
+            if (userAnswer == correctAnswer) {
+                points++;
+            } else {
+                wrongAttempts++;
+                System.out.println("Sai! Dap an la " + correctAnswer);
+            }
+        }
+
+        System.out.println("Ban da ghi duoc tong cong " + points + " diem.");
+        scanner.close();
+    }
+}
